e1aadb82434380ae4190e6704ca11a27cc47021f
Vladimir Postman <v.postman2014@yandex.com>
2016-09-13 21:58:31 +0300

    add 3rd lab

--- src/ru/golovach/core/thread/labs01/Main.java
+++ src/ru/golovach/core/thread/labs01/Main.java
@@ -2,6 +2,7 @@ package ru.golovach.core.thread.labs01;
 
 /**
  * Created by Wladimir on 13.09.2016.
+ * single buffer
  */
 public class Main {
     public static void main(String[] args) {
--- src/ru/golovach/core/thread/labs03/Main.java
+++ src/ru/golovach/core/thread/labs03/Main.java
@@ -1,11 +1,10 @@
-package ru.golovach.core.thread.labs02;
+package ru.golovach.core.thread.labs03;
 
 /**
  * Created by Wladimir on 13.09.2016.
- * craze rabbit
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from labs!");
+        System.out.println("Hello from 3rd lab!");
     }
 }
